08fd03c2302a0954733a4099ee6ce04464c4c5f1
Carrie Estes <my4countrygirl@gmail.com>
2016-08-16 14:30:28 -0600

    static pages & edit feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,20 +11,34 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def create
-    @post = Post.new(post_params)# title body author = params
+  def edit
+    @post = Post.find(params[:id])
+  end
 
-    if @post.save
-      redirect_to posts_path
-    else
-      render :new
-    end
+  def create
+      @post = Post.new(post_params)# title body author = params
+
+      if @post.save
+        redirect_to posts_path(@post)
+      else
+        render :new
+      end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+      if @post.update(post_params)
+      redirect_to post_path(@post)
+      else
+        render :edit
+      end
+
   end
 
   private
 
-  def post_params
-
-    params.require(:post).permit(:title, :author, :body)
-  end
+    def post_params
+      params.require(:post).permit(:title, :author, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  root 'posts#index'
-  resources :posts
-  # get 'posts/index'
-  #
-  # get 'posts/show'
-  #
-  # get 'posts/new'
+  root 'static_pages#home'
 
+get '/about', to: 'static_pages#about'
+
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
